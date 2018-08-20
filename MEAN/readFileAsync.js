db39192cfa942957fdae0e151783a343842e8999
shrubhat <shrubhat55@gmail.com>
2018-08-20 11:38:26 +0530

    "adding modified version of read file program"

--- MEAN/readFileAsync.js
+++ MEAN/readFileAsync.js
@@ -1,17 +1,27 @@
 var shru = require('fs');
-var file = " "
+// var file = " ";
+// console.log(file);
+
+
 console.log("before reading a file");
 
-console.log("Going to get a file")
+console.log("Going to get a file");
 
-var file = shru.readFile('app.js', function(){
-    console.log("Completed reading the file!! Info:  \n");
-});
-function printfile(){
-    if(file!=""){
-        console.log(file);
-    }
+var rfilevalue = false;
+
+var readingfile = function(){
+    console.log("Completed reading the file!! \n");
+     return rfilevalue=true;
 }
 
+var file = shru.readFile('.\\MEAN\\app.js',readingfile);
+
 console.log("End of file");
-printfile();
+
+while(rfilevalue==true){
+        console.log( "writing the file content: \n"+shru.writeFileSync(file,"writing file app.js"));
+}
+
+// do{
+//     console.log( "writing the file content: \n"+file);
+// }while(rfilevalue==true);
--- MEAN/readFileSync.js
+++ MEAN/readFileSync.js
@@ -2,7 +2,7 @@ var shru = require('fs');
 console.log("before reading a file");
 
 console.log("Going to get a file")
-var file = shru.readFileSync('app.js');
+var file = shru.readFileSync('.\\MEAN\\app.js');
 console.log("Completed reading the file!! Info:  \n"+file);
 
 console.log("End of file");
